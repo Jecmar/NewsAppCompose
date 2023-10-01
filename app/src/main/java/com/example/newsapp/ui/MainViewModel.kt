24a5c4c1a91a6f9329658ab4e93c6fc2d856de08
Josue Cruz <jcruz@globalsoft.email>
2023-09-30 22:30:39 -0600

    Created MainViewModel to manage state of splashscreen and destination

---
+++ app/src/main/java/com/example/newsapp/ui/MainViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.newsapp.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newsapp.domain.usecases.AppEntryUseCases
+import com.example.newsapp.ui.navgraph.Route
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    appEntryUseCases: AppEntryUseCases
+): ViewModel() {
+    var splashCondition by mutableStateOf(true)
+        private set
+
+    var startDestination by mutableStateOf(Route.AppStartNavigation.route)
+        private set
+
+    init{
+        appEntryUseCases.readAppEntry().onEach {
+            startDestination = if(it) Route.NewsNavigation.route else Route.AppStartNavigation.route
+            delay(300)
+            splashCondition = false
+        }.launchIn(viewModelScope)
+    }
+}
