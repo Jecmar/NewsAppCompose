c6bb4daa8f701b84b561f3ad776a7da6d6a363c5
Josue Cruz <jcruz@globalsoft.email>
2023-09-30 22:32:05 -0600

    Create graph to set the navigation

---
+++ app/src/main/java/com/example/newsapp/ui/navgraph/NavGraph.kt
@@ -0,0 +1,45 @@
+package com.example.newsapp.ui.navgraph
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import com.example.newsapp.ui.onboarding.OnBoardingScreen
+import com.example.newsapp.ui.onboarding.viewmodel.OnBoardingViewModel
+
+@Composable
+fun NavGraph(
+    startDestination: String
+) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = startDestination) {
+        navigation(
+            route = Route.AppStartNavigation.route,
+            startDestination = Route.OnBoardingScreen.route
+        ) {
+            composable(
+                route = Route.OnBoardingScreen.route
+            ) {
+                val onBoardingViewModel: OnBoardingViewModel = hiltViewModel()
+                OnBoardingScreen(
+                    event = onBoardingViewModel::onEvent
+                )
+            }
+        }
+
+        navigation(
+            route = Route.NewsNavigation.route,
+            startDestination = Route.NewsNavigatorScreen.route
+        ) {
+            composable(
+                route = Route.NewsNavigatorScreen.route
+            ) {
+                Text(text = "News Navigator Screen")
+            }
+        }
+    }
+}
