c65c9f14285875727f436211f9a9d85392db2212
Josue Cruz <jcruz@globalsoft.email>
2023-10-13 22:21:33 -0600

    Created class implementation of NewsRepository to get news response using Pager

---
+++ app/src/main/java/com/example/newsapp/data/remote/repository/NewsRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package com.example.newsapp.data.remote.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.newsapp.data.remote.NewsApi
+import com.example.newsapp.data.remote.NewsPagingSource
+import com.example.newsapp.domain.model.Article
+import com.example.newsapp.domain.repository.NewsRepository
+import com.example.newsapp.utils.Constants.PAGE_SIZE
+import kotlinx.coroutines.flow.Flow
+
+class NewsRepositoryImpl(
+    private val newsApi: NewsApi
+) : NewsRepository {
+    override fun getNews(sources: List<String>): Flow<PagingData<Article>> =
+        Pager(
+            config = PagingConfig(
+                pageSize = PAGE_SIZE
+            ),
+            pagingSourceFactory = {
+                NewsPagingSource(
+                    newsApi,
+                    sources.joinToString()
+                )
+            }
+        ).flow
+}
