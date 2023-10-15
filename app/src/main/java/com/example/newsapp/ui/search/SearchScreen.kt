11e1298a71589c9bc21e16464c56d415df1b5ece
Josue Cruz <jcruz@globalsoft.email>
2023-10-15 16:59:44 -0600

    Created search screen to display news

---
+++ app/src/main/java/com/example/newsapp/ui/search/SearchScreen.kt
@@ -0,0 +1,61 @@
+package com.example.newsapp.ui.search
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.newsapp.data.event.SearchEvent
+import com.example.newsapp.ui.components.ArticlesList
+import com.example.newsapp.ui.components.SearchBar
+import com.example.newsapp.ui.navgraph.Route
+import com.example.newsapp.ui.search.model.SearchState
+import com.example.newsapp.utils.Dimens.MediumPadding1
+
+@Composable
+fun SearchScreen(
+    state: SearchState,
+    event: (SearchEvent) -> Unit,
+    navigate: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(
+                top = MediumPadding1,
+                start = MediumPadding1,
+                end = MediumPadding1
+            )
+            .statusBarsPadding()
+            .fillMaxSize()
+    ) {
+        SearchBar(
+            text = state.searchQuery,
+            readOnly = false,
+            onValueChange = {
+                event(
+                    SearchEvent.UpdateSearchQuery(it)
+                )
+            },
+            onSearch = {
+                event(SearchEvent.SearchNews)
+            }
+        )
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        state.articles?.let{
+            val articles = it.collectAsLazyPagingItems()
+
+            ArticlesList(
+                articles = articles,
+                onClick = {
+                    navigate(Route.DetailsScreen.route)
+                }
+            )
+        }
+    }
+}
